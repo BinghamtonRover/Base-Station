c43f177494ad5fa7f8952fc99d346031c1bb0298
Gold87 <91761103+Gold872@users.noreply.github.com>
2024-11-17 21:21:18 -0500

    Began adding logic for commands

--- lib/src/antenna.dart
+++ lib/src/antenna.dart
@@ -5,24 +5,46 @@ import "collection.dart";
 // TODO: Add AntennaCommand, AntennaData, and Device.ANTENNA to burt_network
 
 class AntennaControl extends Service {
+  /// Serial port for the firmware
   static const port = "/dev/base-station-antenna";
 
+  /// Firmware for the antenna
   final firmware = BurtFirmwareSerial(port: port, logger: logger);
+
+  AntennaCommand _currentCommand = AntennaCommand();
 
   @override
   Future<bool> init() async {
     final result = firmware.init();
     firmware.messages.listen(collection.server.sendWrapper);  // send AntennaData to the Dashboard
-    // collection.server.messages.onMessage<AntennaCommand>(
-    //   name: AntennaCommand().messageName,
-    //   constructor: AntennaCommand.fromBuffer,
-    //    callback: _handleAntennaCommand,
-    // );
+    collection.server.messages.onMessage<AntennaCommand>(
+      name: AntennaCommand().messageName,
+      constructor: AntennaCommand.fromBuffer,
+       callback: _handleAntennaCommand,
+    );
+    collection.server.messages.onMessage<GpsCoordinates>(
+      name: GpsCoordinates().messageName,
+      constructor: GpsCoordinates.fromBuffer,
+      callback: _handleGpsData,
+    );
     return result;
   }
 
   @override
   Future<void> dispose() => firmware.dispose();
 
-  // void _handleAntennaCommand(AntennaCommand command) => firmware.sendMessage(command);
+  void _handleAntennaCommand(AntennaCommand command) {
+    // handshake back to dashboard
+    collection.server.sendMessage(command);
+
+    _currentCommand = command;
+
+    // TODO: Implement logic and stuff
+  }
+
+  void _handleGpsData(GpsCoordinates coordinates) {
+    if (_currentCommand.mode != AntennaControlMode.TRACK_ROVER) {
+      return;
+    }
+  }
 }

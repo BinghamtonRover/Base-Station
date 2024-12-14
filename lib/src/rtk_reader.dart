47feb1673d79f1332e62ef0bd3fa5e7c172c90d0
Gold87 <91761103+Gold872@users.noreply.github.com>
2024-12-13 20:18:29 -0500

    Send multiple packets per read

--- lib/src/rtk_reader.dart
+++ lib/src/rtk_reader.dart
@@ -4,10 +4,13 @@ import "dart:io";
 import "package:base_station/base_station.dart";
 import "package:burt_network/burt_network.dart";
 
-final subsystemsSocket = SocketInfo(address: InternetAddress("192.168.1.20"), port: 8001);
+final subsystemsSocket = SocketInfo(
+  address: InternetAddress("192.168.1.20"),
+  port: 8001,
+);
 
 class RTKReader extends Service {
-  static const rtkPort = "COM11";
+  static const rtkPort = "COM4";
 
   static const _first = 0xD3;
 
@@ -17,6 +20,7 @@ class RTKReader extends Service {
     portName: rtkPort,
     readInterval: const Duration(milliseconds: 10),
     logger: logger,
+    baudRate: 38400,
   );
 
   final List<int> _buffer = [];
@@ -26,50 +30,49 @@ class RTKReader extends Service {
   void _handlePacket(List<int> bytes) {
     _buffer.addAll(bytes);
 
-    if (!_buffer.contains(_first)) {
-      _buffer.clear();
-      return;
+    while (_buffer.contains(_first)) {
+      _buffer.removeRange(0, _buffer.indexOf(_first));
+      if (_buffer.length < 2) {
+        // Wait for more bytes to come in
+        return;
+      }
+
+      // buffer[1] != 0b000000
+      if (_buffer[1] & ~0x03 != 0) {
+        _buffer.removeRange(0, 2);
+        return;
+      }
+
+      if (_buffer.length < 4) {
+        // wait for more bytes to come in
+        return;
+      }
+
+      final size = (_buffer[1] << 8) | _buffer[2];
+
+      if (_buffer.length < size + 3) {
+        // wait for the payload to come in
+        return;
+      }
+
+      if (_buffer.length < 3 + size + 3 + 1) {
+        // wait for the crc
+        return;
+      }
+
+      final endIndex = 3 + size + 3;
+
+      final List<int> fullPacket = _buffer.sublist(0, endIndex + 1);
+
+      _buffer.removeRange(0, endIndex + 1);
+
+      logger.debug("Decoded ${fullPacket.length} byte RTCM packet");
+
+      final message = RoverPosition(rtkMessage: fullPacket);
+
+      collection.server.sendMessage(message, destination: subsystemsSocket);
     }
-    _buffer.removeRange(0, _buffer.indexOf(_first));
-    if (_buffer.length < 2) {
-      // Wait for more bytes to come in
-      return;
-    }
-
-    // buffer[1] != 0b000000
-    if (_buffer[1] & ~0x03 != 0) {
-      _buffer.removeRange(0, 2);
-      return;
-    }
-
-    if (_buffer.length < 4) {
-      // wait for more bytes to come in
-      return;
-    }
-
-    final size = (_buffer[1] << 8) | _buffer[2];
-
-    if (_buffer.length < size + 3) {
-      // wait for the payload to come in
-      return;
-    }
-
-    if (_buffer.length < 3 + size + 3 + 1) {
-      // wait for the crc
-      return;
-    }
-
-    final endIndex = 3 + size + 3;
-
-    final List<int> fullPacket = _buffer.sublist(0, endIndex + 1);
-
-    _buffer.removeRange(0, endIndex + 1);
-
-    logger.debug("Decoded ${_buffer.length} RTCM packet");
-
-    final message = RoverPosition(rtkMessage: fullPacket);
-
-    collection.server.sendMessage(message, destination: subsystemsSocket);
+    _buffer.clear();
   }
 
   @override
